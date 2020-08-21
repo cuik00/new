5bc52b11f6b839895eef1ea928cf34be82f99b4e
cuik00 <37495100+cuik00@users.noreply.github.com>
2020-08-21 07:40:41 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+account = {
+	"Username":"M1F74H",
+	"Password":"M1F74H4054"
+}
+license = "xxx"
+tradeset = {
+	"BaseTrade":"0.005",
+	"C1":"40", 					#5-95
+	"C2":"49", 					#5-95
+	"NumberOfTrade":"3", 				#max limit 200
+	"MultiplyOnWin":"1", 				## 0 untuk OFF 
+	"MultiplyOnLose":"0.1", 				## 0 untuk OFF 
+	"MaxBaseTrade":"0",				# 0 untuk OFF
+	"ResetOnLoseMaxTrade":"ON", 			# ON/OFF
+	"StopOnLoseMaxTrade":"ON",			# ON/OFF
+	"TargetProfit":"50",				#0 untuk OFF
+	"ClientSeed":"9999",				#max 32 digits
+	"RecoveryMultiplier":"2.2",			# 1 untuk OFF
+	"RecoveryIncrease":"0"	 			# 0 untuk OFF. Increase your BaseTrade If Lose
+}
+
+tools = {
+	"AddDelayTrade":"0",				#Delay Per Trade
+	"AddDelayTradeWin":"0",				#Delay Per Trade Win
+	"AddDelayTradeLose":"0"				#Delay Per Trade Lose
+}
