ee37d402567add1d2de95753e3e29aa3e8f99578
cuik00 <37495100+cuik00@users.noreply.github.com>
2020-08-25 21:58:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ account = {
 license = "STB-0MCG3-4KJ9I-3EILI-DVC"
 tradeset = {
 	"BaseTrade":"0.01",
-	"C1":"49", 					#5-95
-	"C2":"54", 					#5-95
-	"NumberOfTrade":"3", 				#max limit 200
+	"C1":"11.12", 					#5-95
+	"C2":"15.15", 					#5-95
+	"NumberOfTrade":"1", 				#max limit 200
 	"MultiplyOnWin":"1", 				## 0 untuk OFF 
 	"MultiplyOnLose":"0.01", 				## 0 untuk OFF 
 	"MaxBaseTrade":"0",				# 0 untuk OFF
 	"ResetOnLoseMaxTrade":"ON", 			# ON/OFF
 	"StopOnLoseMaxTrade":"ON",			# ON/OFF
-	"TargetProfit":"5000",				#0 untuk OFF
+	"TargetProfit":"500",				#0 untuk OFF
 	"ClientSeed":"9999",				#max 32 digits
-	"RecoveryMultiplier":"2.2",			# 1 untuk OFF
+	"RecoveryMultiplier":"1.1789",			# 1 untuk OFF
 	"RecoveryIncrease":"0"	 			# 0 untuk OFF. Increase your BaseTrade If Lose
 }
 
